e418cad52dcb6c4f9bd50c27e6ba5756d428160b
trieuvy793 <trieuvy091703@gmail.com>
2024-07-21 03:02:09 +0700

    added: get forecasts

--- src/controllers/weather.controller.js
+++ src/controllers/weather.controller.js
@@ -33,4 +33,55 @@ const getLocations = async (req, res) => {
   }
 };
 
-module.exports = { getLocations };
+const getCurrentWeather = async (req, res) => {
+  let { location } = req.query;
+  try {
+    if (!location) {
+      location = await getUserIP();
+    }
+
+    const response = await axios.get('http://api.weatherapi.com/v1/current.json', {
+      params: {
+        key: process.env.WEATHER_API_KEY,
+        q: location
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    if (error.response) {
+      const { status, data } = error.response;
+      res.status(status).json(data);
+    } else {
+      res.status(500).json({ error: 'Error fetching current weather data' });
+    }
+  }
+};
+
+const getForecasts = async (req, res) => {
+  let { location, days } = req.query;
+  try {
+    if (!location) {
+      location = await getUserIP();
+    }
+
+    const response = await axios.get('http://api.weatherapi.com/v1/forecast.json', {
+      params: {
+        key: process.env.WEATHER_API_KEY,
+        q: location,
+        days,
+        aqi: 'no',
+        alerts: 'no'
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    if (error.response) {
+      const { status, data } = error.response;
+      res.status(status).json(data);
+    } else {
+      res.status(500).json({ error: 'Error fetching forecast data' });
+    }
+  }
+};
+
+module.exports = { getLocations, getCurrentWeather, getForecasts };
--- src/routes/weather.route.js
+++ src/routes/weather.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { getLocations } = require('../controllers/weather.controller');
+const { getLocations, getCurrentWeather, getForecasts } = require('../controllers/weather.controller');
 const router = express.Router();
 
 router.get('/search', getLocations);
+router.get('/current', getCurrentWeather);
+router.get('/forecasts', getForecasts);
 
 module.exports = router;

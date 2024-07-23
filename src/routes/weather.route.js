0b201a9749c4a7c029ff4c6cf26302479ab57b77
trieuvy793 <trieuvy091703@gmail.com>
2024-07-23 09:26:31 +0700

    fixed: locations, forecasts

--- src/routes/weather.route.js
+++ src/routes/weather.route.js
@@ -4,9 +4,9 @@ const weatherController = require('../controllers/weather.controller');
 const { authenticateToken } = require('../middlewares/auth.middleware');
 
 router.post('/generate-token', weatherController.generateTokenController);
-router.get('/locations', authenticateToken, weatherController.getLocations);
+router.get('/locations', weatherController.getLocations);
 router.get('/current-weather', authenticateToken, weatherController.getCurrentWeather);
-router.get('/forecasts', authenticateToken, weatherController.getForecasts);
+router.get('/forecasts', weatherController.getForecasts);
 router.get('/history', authenticateToken, weatherController.getHistory);
 
 module.exports = router;
